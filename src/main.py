5e1eef92b2173227ea673571fdfa193256f919d9
Pawarasasmina <pawarasasmina2002@icloud.com>
2025-08-01 02:58:04 +0530

    aaa

--- src/main.py
+++ src/main.py
@@ -8,7 +8,8 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, supports_credentials=True)
+CORS(app, resources={r"/*": {"origins": "https://photobooth-frontend.vercel.app"}})
+
 
 
 # DON'T CHANGE THIS !!!
